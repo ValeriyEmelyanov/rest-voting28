bb7568c98a173ccf55c85f2e8d11f45185d2766a
Valeriy Emelyanov <emelva@rambler.ru>
2023-03-20 20:07:46 +0300

    Init

---
+++ src/main/java/com/example/restvoting28/RestVoting28Application.java
@@ -0,0 +1,13 @@
+package com.example.restvoting28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestVoting28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestVoting28Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restvoting28/RestVoting28ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restvoting28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestVoting28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
